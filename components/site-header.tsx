84ca20423d23487fa755ca591eafcddb693a6596
Palm Civet <palmcivet_email@qq.com>
2025-05-30 10:29:08 +0800

    feat: 更新 SiteHeader 组件的样式，添加 border-grid 类；在 globals.css 中为按钮添加光标样式

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -39,7 +39,7 @@ export async function SiteHeader() {
   return (
     <header
       className={cn(
-        "supports-backdrop-blur:bg-background/90 sticky top-0 z-40 w-full border-b border-border bg-background/40 backdrop-blur-lg",
+        "supports-backdrop-blur:bg-background/90 sticky top-0 z-40 w-full border-b border-border bg-background/40 backdrop-blur-lg border-grid",
       )}
     >
       <div className="container flex h-16 items-center">
