b75043297895652458cb12a5796d0af26f697505
overtrue <anzhengchao@gmail.com>
2025-07-07 16:01:10 +0800

    wip

---
+++ registry/default/lib/index.ts
@@ -0,0 +1,4 @@
+export * from "./generate-example";
+export * from "./resolve-ref";
+export * from "./schema-utils";
+export * from "./type-utils";
---
+++ registry/default/lib/schema-utils.ts
@@ -0,0 +1,39 @@
+/**
+ * Check if a schema is a reference object
+ */
+export function isReferenceObject(obj: any): obj is { $ref: string } {
+  return obj && typeof obj === "object" && "$ref" in obj;
+}
+
+/**
+ * Check if a schema is a schema object
+ */
+export function isSchemaObjectNotRef(obj: any): boolean {
+  return obj && typeof obj === "object" && !("$ref" in obj);
+}
+
+/**
+ * Get the type of a schema
+ */
+export function getSchemaType(schema: any): string {
+  if (schema.type) {
+    return schema.type;
+  }
+  if (schema.allOf) {
+    return "allOf";
+  }
+  if (schema.oneOf) {
+    return "oneOf";
+  }
+  if (schema.anyOf) {
+    return "anyOf";
+  }
+  return "object";
+}
+
+/**
+ * Check if a property is required in a schema
+ */
+export function isPropertyRequired(schema: any, propertyName: string): boolean {
+  return schema.required?.includes(propertyName) || false;
+}
